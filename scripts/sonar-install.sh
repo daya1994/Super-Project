8394b6f473a84f57e3116cde4ba454d15e117fa8
DAYANAND DEEPAK <30375388+daya1994@users.noreply.github.com>
2026-09-11 20:22:10 +0530

    Update sonar-install.sh

--- scripts/sonar-install.sh
+++ scripts/sonar-install.sh
@@ -8,7 +8,7 @@ NOTE: Make sure you add the password of each user like ec2, sonarqube and root
 
 ## 1.. Install jdk 17
 
-sudo yum install java-17-amazon-corretto-devel
+sudo yum install java-21-amazon-corretto-devel -y
 
 ##  2.. Add sonarqube as user as for sonarqube specific user is required (make sure u set password using sudo passwd)
 sudo useradd sonarqube
@@ -17,25 +17,30 @@ sudo useradd sonarqube
 
 ## down the sonarqube community verison using tghe wget command
 
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.1.0.73491.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-26.9.0.129388.zip
 
 ## 4. move it /opt
 
-sudo mv sonarqube-10.1.0.73491.zip /opt/sonarqube-10.1.0.73491.zip
+sudo mv sonarqube-26.9.0.129388.zip /opt/
 cd /opt
 
 ##  5.. unzip th folder
-sudo unzip sonarqube-10.1.0.73491.zip 
+sudo unzip sonarqube-26.9.0.129388.zip
 
 ## 6.. change the ownership of sonarqube 
-sudo chown -R sonarqube:sonarqube sonarqube-10.1.0.73491
+sudo chown -R sonarqube:sonarqube /opt/sonarqube-26.9.0.129388
+sudo chmod -R 755 /opt/sonarqube-26.9.0.129388
 su sonarqube
 
 ## 7.. start the server 
-cd sonarqube-10.1.0.73491/
+cd sonarqube-26.9.0.129388/
 cd bin
 cd linux-x86-64/
 ./sonar.sh start
+##/opt/sonarqube-26.9.0.129388/bin/linux-x86-64/sonar.sh start
 
 ## 8.. check the status using 
 ./sonar.sh status
+
+## Check logs:
+tail -f /opt/sonarqube-26.9.0.129388/logs/sonar.log
