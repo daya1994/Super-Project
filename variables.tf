3a3e49d2fbe43c34b540efde33e0e9f3e101c88f
DAYANAND DEEPAK <30375388+daya1994@users.noreply.github.com>
2026-09-13 16:29:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default = "us-east-1c"
 }
 
 variable "aws_type" {
   description = "AWS ec2 type"
-  default = "c7i-flex.large"
+  default = "m7i-flex.large"
 }
 
 variable "aws_ami" {
